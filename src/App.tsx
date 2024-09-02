79381a659bee7ce97cfeed280b73ff8c0a15f5e8
Aaron Müller <160637865+amueller0@users.noreply.github.com>
2024-09-02 19:10:13 +0200

    style: brilliance black theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { useState } from "react"
+import brillianceBlackThemeData from "../res/themes/brilliance-black.json"
+import { useEffect, useState } from "react"
 import EditorPanel from "./components/EditorPanel"
 import PreviewPanel from "./components/PreviewPanel"
+import { useMonaco } from "@monaco-editor/react"
 
 function App() {
     const [markdownString, setMarkdownString] = useState<string>("")
+    const monaco = useMonaco()
+
+    useEffect(() => {
+        if (!monaco) return
+
+        /** Brilliance Theme (https://github.com/brijeshb42/monaco-themes) */
+        /** @ts-ignore */
+        monaco.editor.defineTheme("brilliance-black", brillianceBlackThemeData)
+        monaco.editor.setTheme("brilliance-black")
+    }, [monaco])
 
     return (
         <div className="grid grid-cols-2 w-dvw h-dvh">
